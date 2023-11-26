333465c78550d4bcb7dd5c5cc91cb999f6a7d934
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2023-11-26 20:40:03 +0100

    Create show tasks function

--- src/creatingTask.js
+++ src/creatingTask.js
@@ -29,9 +29,11 @@ function createNewTask() {
 
     const newTask = CreateTask(taskTitle.value, taskDescription.value, taskDate.value);
     allTasks.push(newTask);
-    taskTitle.value = "";
-    taskDescription.value = "";
-    taskDate.value = "";    
+    //taskTitle.value = "";
+    //taskDescription.value = "";
+    //taskDate.value = "";    
+    closeForm();
+    showTasks();
 }
 
 function showForm() {
@@ -42,6 +44,67 @@ function showForm() {
 function closeForm() {
     const taskForm = document.querySelector(".add-task-form");
     taskForm.style.display = "none"
+}
+
+function showTasks() {
+    const tasksContainer = document.querySelector(".tasks-container");
+    tasksContainer.innerHTML = ""
+
+    for (let i = 0; i < allTasks.length; i++) {
+        const taskDiv = document.createElement("div");
+        taskDiv.classList.add("task");
+
+        const circle = document.createElement("span");
+        circle.classList.add("circle");
+
+        const taskInfo = document.createElement("div");
+        taskInfo.classList.add("task-info");
+
+        const taskTitle = document.createElement("div");
+        taskTitle.classList.add("task-title");
+        taskTitle.textContent = allTasks[i].title;
+
+        const taskDescription = document.createElement("div");
+        taskDescription.classList.add("task-description");
+        taskDescription.textContent = allTasks[i].description;
+
+        const taskDate = document.createElement("div");
+        taskDate.classList.add("date");
+        taskDate.textContent = allTasks[i].date;
+
+        const importantStar = document.createElement("div");
+        importantStar.classList.add("important-star");
+        importantStar.textContent = "Star"
+
+        const editIcons = document.createElement("div");
+        editIcons.classList.add("edit-icons");
+        for (let j = 0; j < 3; j++) {
+            const dot = document.createElement("span");
+            dot.classList.add("dot");
+            editIcons.appendChild(dot);
+        }
+
+        const optionsButtons = document.createElement("div");
+        optionsButtons.classList.add("options-buttons");
+
+        const deleteTaskButton = document.createElement("button");
+        deleteTaskButton.classList.add("delete-project-button");
+        deleteTaskButton.textContent = "Delete";
+
+        const editTaskButton = document.createElement("button");
+        editTaskButton.classList.add("rename-project-button");
+        editTaskButton.textContent = "Edit";
+
+        taskDiv.appendChild(circle);
+        taskInfo.appendChild(taskTitle);
+        taskInfo.appendChild(taskDescription);
+        taskDiv.appendChild(taskInfo);
+        taskDiv.appendChild(taskDate);
+        taskDiv.appendChild(importantStar);
+        taskDiv.appendChild(editIcons);
+        tasksContainer.appendChild(taskDiv);
+
+    }
 }
 
 
